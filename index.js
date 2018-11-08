6642da5b88e269de8a11eef24859af04dfae75b4
orksbane45 <39961514+orksbane45@users.noreply.github.com>
2018-11-08 20:35:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 	var prefix = "=";
 
-	client.login("NTA5NjI1MzQ4NzQxMjY3NDgx.DsQihw.3lYLjjm7IIVpAEJk52n185kdGHg");
+	client.login(process.env.);
 
 	client.on("ready", () => {
 		console.log("Bot connected");
